e50194721eafb11e95fc29672a93778406071404
NatyaHans <49882391+NatyaHans@users.noreply.github.com>
2021-12-02 17:30:31 -0500

    Update slurm_4_tania_raxml.sh

--- files/slurm_4_tania_raxml.sh
+++ files/slurm_4_tania_raxml.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-#SBATCH --job-name=raxmltania  #Job name
-#SBATCH --mail-type=ALL # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=pinedae@ufl.edu # Where to send mail
-#SBATCH --cpus-per-task=4 # Number of cores: Can also use -c=4
+#SBATCH --job-name=raxmlcoreNH  #Job name
+#SBATCH --mail-type=FAIL # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=USERNAME@ufl.edu # Where to send mail
+#SBATCH --cpus-per-task=1 # Number of cores: Can also use -c=4
 #SBATCH --mem-per-cpu=1gb # Per processor memory can be increase 8gb
 #SBATCH -t 18:00:00     # Walltime
 #SBATCH -o raxml_output.%j.out # Name output file
-#SBATCH
-#SBATCH --account=paulay
+#SBATCH --account=GROUPNAME
+#SBATCH --qos=GROUPNAME
 
 pwd; hostname; date
 echo Working directory is $SLURM_SUBMIT_DIR
@@ -18,5 +18,5 @@ echo There are $SLURM_CPUS_ON_NODE cores available.
 module load raxml
 
 # do 1000 bootstraps + likelihood search for the single best tree
-raxmlHPC-PTHREADS-SSE3 -f a -m GTRGAMMA -s YOURALIGNMENTSINPHYLIP.phy -p $RANDOM -x $RANDOM -N 1000 -n RAxML_raxmloutput_tania -T $SLURM_TASKS_PER_NODE
+raxmlHPC-SSE3 -f a -m GTRGAMMA -s YOURALIGNMENTSINPHYLIP.phy -p $RANDOM -x $RANDOM -N 1000 -n RAxML_raxmloutput -T $SLURM_TASKS_PER_NODE
 
